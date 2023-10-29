4195a022614cb72290b236a2d9c31364690adae8
Emythea <79308834+tuna-sahin@users.noreply.github.com>
2023-10-29 03:14:32 +0300

    Add files via upload

--- Lab 5/binary2cathode.vhd
+++ Lab 5/binary2cathode.vhd
@@ -1,27 +1,34 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity b2c_test is
-end b2c_test;
+entity binary2cathode is
+  Port (in1 : in std_logic_vector(3 downto 0);
+        out1: out std_logic_vector(6 downto 0));
+end binary2cathode;
 
-architecture Behavioral of b2c_test is
-    component binary2cathode is
-        Port (in1 : in std_logic_vector(3 downto 0);
-              out1: out std_logic_vector(6 downto 0));
-    end component;
-    
-    signal in1 : std_logic_vector(3 downto 0);
-    signal out1: std_logic_vector(6 downto 0);     
+architecture Behavioral of binary2cathode is
+
 begin
-    b2c1 : binary2cathode port map(in1 => in1,out1 => out1);
-    process
+    process(in1)
     begin
-        in1 <= "1010";
-        wait for 100ns;
-        in1 <= "0000";
-        wait for 100ns;
-        in1 <= "0010";
-        wait for 100ns;
+        case in1 is
+        when "0000" => out1 <= "0000001"; -- "0"     
+        when "0001" => out1 <= "1001111"; -- "1" 
+        when "0010" => out1 <= "0010010"; -- "2" 
+        when "0011" => out1 <= "0000110"; -- "3" 
+        when "0100" => out1 <= "1001100"; -- "4" 
+        when "0101" => out1 <= "0100100"; -- "5" 
+        when "0110" => out1 <= "0100000"; -- "6" 
+        when "0111" => out1 <= "0001111"; -- "7" 
+        when "1000" => out1 <= "0000000"; -- "8"     
+        when "1001" => out1 <= "0000100"; -- "9" 
+        when "1010" => out1 <= "0000010"; -- a
+        when "1011" => out1 <= "1100000"; -- b
+        when "1100" => out1 <= "0110001"; -- C
+        when "1101" => out1 <= "1000010"; -- d
+        when "1110" => out1 <= "0110000"; -- E
+        when others => out1 <= "0111000"; -- F
+        end case;
     end process;
-    
+
 end Behavioral;
